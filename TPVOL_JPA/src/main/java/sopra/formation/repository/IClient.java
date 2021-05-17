aeffcb8d9fd4e88c361829c3db0ca18a6e6037f2
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-17 10:54:36 +0200

    Renamecomplete

--- TPVOL_JPA/src/main/java/sopra/formation/repository/IClient.java
+++
@@ -1,7 +0,0 @@
-package sopra.formation.repository;
-
-import sopra.vol.model.Client;
-
-public interface IClient extends IRepository<Client, Integer> {
-
-}
--- TPVOL_JPA/src/main/java/sopra/formation/repository/ICompagnieAerienne.java
+++
@@ -1,7 +0,0 @@
-package sopra.formation.repository;
-
-import sopra.vol.model.CompagnieAerienne;
-
-public interface ICompagnieAerienne extends IRepository<CompagnieAerienne, String> {
-
-}
