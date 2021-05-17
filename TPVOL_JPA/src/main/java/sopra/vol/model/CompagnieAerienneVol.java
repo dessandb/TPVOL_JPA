64d1294a021f0e4f68bd4426be156ee788431ad1
camille <c>
2021-05-17 14:22:45 +0200

    on va y arriver !

--- TPVOL_JPA/src/main/java/sopra/vol/model/CompagnieAerienneVol.java
+++ TPVOL_JPA/src/main/java/sopra/vol/model/CompagnieAerienneVol.java
@@ -18,12 +18,8 @@ public class CompagnieAerienneVol {
 	@ManyToOne
 	@JoinColumn(name = "Compagnie_Aerienne_Code")
 	private CompagnieAerienne compagnieAerienne;
-<<<<<<< HEAD
-	@OneToMany(mappedBy="vols")
-=======
 	@ManyToOne
 	@JoinColumn(name = "Vol_Id")
->>>>>>> main
 	private Vol vol;
 
 	public CompagnieAerienneVol() {
