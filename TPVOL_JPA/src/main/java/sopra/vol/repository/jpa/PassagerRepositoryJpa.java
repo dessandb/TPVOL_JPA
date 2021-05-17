cd6db992b5153c4bb0f4d2b8ca28865612f5a0c7
PECQUE <aubeline.pecque@hotmail.com>
2021-05-17 12:59:12 +0200

    client jpa passager jpa et application

--- TPVOL_JPA/src/main/java/sopra/vol/repository/jpa/PassagerRepositoryJpa.java
+++ TPVOL_JPA/src/main/java/sopra/vol/repository/jpa/PassagerRepositoryJpa.java
@@ -1,5 +1,133 @@
 package sopra.vol.repository.jpa;
 
-public class PassagerRepositoryJpa {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.vol.Application;
+import sopra.vol.model.Passager;
+import sopra.vol.repository.IPassagerRepository;
+
+public class PassagerRepositoryJpa implements IPassagerRepository {
+
+	@Override
+	public List<Passager> findAll() {
+		List<Passager> passagers = new ArrayList<Passager>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Passager> query = em.createQuery("select e from Passager e", Passager.class);
+
+			passagers = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return passagers;
+	}
+
+	@Override
+	public Passager findById(Integer id) {
+		Passager passager = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			passager = em.find(Passager.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return passager;
+	}
+
+	@Override
+	public Passager save(Passager obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	@Override
+	public void delete(Passager obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+	}
 
 }
